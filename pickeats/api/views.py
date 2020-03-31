2b868d56da5142abde052826341ad7fce1bd198e
Derek Xia <derek.xia@gmail.com>
2020-03-30 22:31:55 -0700

    Added example for mongodb

--- pickeats/api/views.py
+++ pickeats/api/views.py
@@ -1,7 +1,20 @@
+# from pymongo import MongoClient
 from rest_framework import viewsets, permissions
 
 from .serializers import TodoSerializer
 # from todos.models import Todo
+
+
+# client = MongoClient('mongodb://127.0.0.1:27017/') # TODO: Change this to actual IP and port
+# db = client.pickeats # TODO: Change this to actual mongodb database name
+
+"""
+Example view that queries mongodb
+
+class YelpDataList(APIView):
+    def get(self, request, format=None):
+        return Response([d for d in db.restaurant.find({},{'_id':0})])
+"""
 
 
 class TodoViewSet(viewsets.ModelViewSet):
