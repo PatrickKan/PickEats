c55fb615498c2456edeaad547c7d9bdd15f463cc
david-wang24 <daiweiw2@illinois.edu>
2020-04-07 21:52:17 -0700

    post requests

--- frontend/src/components/todos/TodoForm.js
+++ frontend/src/components/todos/TodoForm.js
@@ -56,6 +56,15 @@ class TodoForm extends Component {
         "type": "text",
         "name": "question3",
         "title": "How far are you willing to go?",
+        "requiredErrorText": "Please enter a value between 0 and 24.",
+        "validators": [
+         {
+          "type": "numeric",
+          "minValue": 0,
+          "maxValue": 24
+         }
+        ],
+        "inputType": "number",
         "placeHolder": "enter distance in miles"
        },
        {
@@ -85,28 +94,72 @@ class TodoForm extends Component {
       }
     };
   
-    const body = {
-      description: JSON.stringify(survey.data.question1)
+    // preferences
+    const body1 = {
+      description: survey.data.question1
     }
 
-    print(survey.data.question1)
+    // profile: price range, radius
+    var one = survey.data.question2.includes("item1")
+    var two = survey.data.question2.includes("item2")
+    var three = survey.data.question2.includes("item3")
+    var four = survey.data.question2.includes("item4")
+
+    console.log(one)
+    console.log(two)
+    console.log(three)
+    const body2 = {
+      radius: Math.round(survey.data.question3 * 1609.34),
+      price_1:one,
+      price_2:two,
+      price_3:three,
+      price_4:four
+    }
+
+    // nutritional goals
+    const body3 = {
+      description: survey.data.question4
+    }
+
+    //allergies
+    const body4 = {
+      description: survey.data.question5
+    }
+
 
     if (token) {
       config.headers['Authorization'] = `Token ${token}`;
     }
+
     console.log(token);
-    axios.post('http://localhost:8000/api/user/prefers/', body, config).then(function (response) { 
+
+    axios.post('/api/user/prefers/', body1, config).then(function (response) { 
       console.log(response);
     });
+
+    axios.patch('/api/user/profile/', body2, config).then(function (response) { 
+      console.log(response);
+    });
+
+    axios.post('/api/user/goals/', body3, config).then(function (response) { 
+      console.log(response);
+    });
+
+    axios.post('/api/user/allergies/', body4, config).then(function (response) { 
+      console.log(response);
+    });
+
+
+    
 
     console.log("Survey results: " + JSON.stringify(survey.data));
     this.setState({completed: true});
    }
 
    render() {
-    if(this.state.completed) {
-      return <Redirect to='/' />;
-    }
+    // if(this.state.completed) {
+    //   return <Redirect to='/' />;
+    // }
 
     //Create the model and pass it into react Survey component
     //You may create survey model outside the render function and use it in your App or component
