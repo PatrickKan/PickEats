3232de90e754e2a66fa798489b31fb5dcff6fda7
Derek Xia <derek.xia@gmail.com>
2020-04-28 18:04:41 -0700

    Added previous and next buttons

--- frontend/src/components/yelp/RecPage.js
+++ frontend/src/components/yelp/RecPage.js
@@ -44,10 +44,15 @@ import { getRecommendations } from '../../actions/yelp';
 import { geolocated } from "react-geolocated";
 import LocationTracker from './Location';
 import { FaUtensils } from "react-icons/fa";
-import { Rating } from 'semantic-ui-react';
+import { Rating, Card } from 'semantic-ui-react';
 import StarRatings from 'react-star-ratings';
 
+import { Form, Grid, List, Transition } from 'semantic-ui-react'
+
 class RecPage extends Component {
+  state = {
+    index: 0,
+  }
   componentDidMount() {
     this.props.getRecommendations(0).then(() => {
       // console.log("force updating")
@@ -67,48 +72,105 @@ class RecPage extends Component {
       objectFit: "cover"
   }
 
+  handlePrevious = () =>
+    this.setState((prevState) => prevState.index > 0 ? { index: prevState.index - 1} : {index: 0})
+  
+  handleNext = () =>
+    this.setState((prevState) => {
+      if (prevState.index + 1 >= this.props.recs.length) {
+        this.props.getRecommendations(this.props.recs.length);
+        console.log('next offset: ' + this.props.recs.length);
+      }
+      return { index: prevState.index + 1};
+    })
+
+  renderRec(rec) {
+    return (
+      rec.name ? 
+      ( <div className='ui card' key={rec.id}>
+          <div className="image">
+            <img style={this.imgStyle} src={rec.image_url}/>
+          </div>
+          <div className='content'>
+            <div className="header">{rec.name}</div>
+            <div className="meta">{rec.price}</div>
+            <div className='description'><FaUtensils/>{"  " + rec.location.address1 + " "}</div>
+            <div className='description'>
+              {rec.rating.toFixed(1) + " "}  
+              <StarRatings
+              rating={rec.rating}
+              starRatedColor="black"
+              numberOfStars={5}
+              starDimension='15px'
+              starSpacing='1px'
+              name='rating'
+              />
+              {" " + rec.review_count + "  reviews"}</div>
+          </div>
+          <div className="extra content">
+            <a>
+              {this.metersToMiles(rec.distance) + " miles away"} 
+            </a>
+          </div>
+        </div> ) : (<div/>)
+    )
+  }
+
   render() {
     return (
       <div>
-        <div className='ui cards' style={{ marginTop: '2rem' }}>
-          {this.props.recs.map(rec => (
-            rec.name ? 
-            ( <div className='ui card' key={rec.id}>
-                <div class="image">
-                  <img style={this.imgStyle} src={rec.image_url}/>
-                </div>
-                <div className='content'>
-                  <div className="header">{rec.name}</div>
-                  <div className="meta">{rec.price}</div>
-                  <div className='description'><FaUtensils/>{"  " + rec.location.address1 + " "}</div>
-                  <div className='description'>
-                    {rec.rating.toFixed(1) + " "}  
-                    <StarRatings
-                    rating={rec.rating}
-                    starRatedColor="black"
-                    numberOfStars={5}
-                    starDimension='15px'
-                    starSpacing='1px'
-                    name='rating'
-                    />
-                    {" " + rec.review_count + "  reviews"}</div>
-                </div>
-                <div className="extra content">
-                  <a>
-                    {this.metersToMiles(rec.distance) + " miles away"} 
-                  </a>
-                </div>
-              </div> ) : (<div/>)
-          ))}
-        </div>
+        <Grid>
+          <Grid.Row columns={3}>
+            <Grid.Column>
+            </Grid.Column>
+            <Grid.Column>
+              {/* <Transition.Group
+                as={List}
+                animation="browse"
+                duration={500}
+              > */}
+                {<List.Item key={this.state.index}>
+                  <List.Content>
+                  <div className='ui cards' style={{ marginTop: '2rem' }}>
+                    {this.state.index < this.props.recs.length ? this.renderRec(this.props.recs[this.state.index]) : null}
+                  </div>
+                  </List.Content>
+                </List.Item>}
+              {/* </Transition.Group> */}
+            </Grid.Column>
+            <Grid.Column>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row columns={6}>
+            <Grid.Column/>
+            <Grid.Column/>
+
+            <Grid.Column textAlign='center'>
+              <Form.Button
+                content="Previous"
+                onClick={this.handlePrevious}
+              />
+            </Grid.Column>
+            <Grid.Column textAlign='left'>
+              <Form.Button
+                content="Next"
+                onClick={this.handleNext}
+              />
+            </Grid.Column>
+
+            <Grid.Column/>
+            <Grid.Column/>
+          </Grid.Row>
+        </Grid>
         <LocationTracker/>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  recs: Object.values(state.todos)
+const mapStateToProps = (state, ownProps) => ({
+  recs: ownProps.recs ? ownProps.recs.concat(Object.values(state.todos)) : Object.values(state.todos)
 });
 
 export default connect(
